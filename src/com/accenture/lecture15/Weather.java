d1ca1458e4e1abfcd947e95fbda56afe728c5510
Ildziite <ilga.baiza@gmail.com>
2019-11-23 13:36:52 +0200

    Task7

--- src/com/accenture/lecture15/Weather.java
+++ src/com/accenture/lecture15/Weather.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Weather {
 
 	final String country;
-	
 
 	int[] daysWeather;
 
@@ -13,16 +12,27 @@ public class Weather {
 		this.country = country;
 		this.daysWeather = daysWeather;
 	}
-	
+
 	public int[] getDaysWeather() {
 		return daysWeather;
 	}
+
 	public String getCountry() {
 		return country;
 	}
-	public String toString(){
-		return "Weather forecast in '"+this.country+"' for next days '"+ Arrays.toString(this.getDaysWeather()) +"'";
+
+	public String toString() {
+		return "Weather forecast in '" + this.country + "' for next days '"
+				+ Arrays.toString(this.getDaysWeather()) + "'";
 	}
 
-	
+	public double getWeekAverage() {
+		double sum = 0;
+		for (int i = 0; i < this.daysWeather.length; i++) {
+			sum = sum + this.daysWeather[i];
+		}
+		double average = sum / this.daysWeather.length;
+		return average;
+	}
+
 }
