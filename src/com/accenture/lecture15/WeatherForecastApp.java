3898c7844dcdc68ed57a8a234f23738544b85079
Ildziite <ilga.baiza@gmail.com>
2019-11-25 19:57:52 +0200

    WeatherForecastApp update

--- src/com/accenture/lecture15/WeatherForecastApp.java
+++ src/com/accenture/lecture15/WeatherForecastApp.java
@@ -8,7 +8,7 @@ public class WeatherForecastApp {
 	static ArrayList<Weather> forecastList = new ArrayList<>();
 
 	public static void main(String[] args) {
-		
+
 		forecastList.add(Weather.getWeatherForecast("Estonia"));
 		forecastList.add(Weather.getWeatherForecast("Finland"));
 		forecastList.add(Weather.getWeatherForecast("Sweden"));
@@ -45,12 +45,6 @@ public class WeatherForecastApp {
 			default:
 				System.out.println("Option not recognized!");
 			}
-			System.out
-					.println("Do you want to chosse another option? [y]-yes,[n]-no");
-			String next = input.nextLine();
-			if (next == "n") {
-				break;
-			}
 		}
 	}
 
@@ -60,29 +54,31 @@ public class WeatherForecastApp {
 		}
 		System.out.println();
 	}
-	
-	public static void addNewForecast(){
+
+	public static void addNewForecast() {
 		System.out.println("Input country for new forecast");
 		String country = input.nextLine();
 		System.out.println("Input temperatures for new forecast");
-		int[] forecast=new int[7];
-		for(int i=0;i<7;i++){
+		int[] forecast = new int[7];
+		for (int i = 0; i < 7; i++) {
 			int temperature = input.nextInt();
-			forecast[i]=temperature;
+			forecast[i] = temperature;
 		}
-		
+
 		Weather NewForecast = new Weather(country, forecast);
 		forecastList.add(NewForecast);
 	}
-	
-	public static void deleteForecast(){
+
+	public static void deleteForecast() {
 		System.out.println("Input country to delete from forecast");
 		String country = input.nextLine();
 		for (int i = 0; i < forecastList.size(); i++) {
-			if (forecastList.get(i).country==country){
-				System.out.println("yeey index ir " + i);
+			if (forecastList.get(i).country.equals(country)) {
+				forecastList.remove(i);
+				System.out.println("Country " + country
+						+ " has been sucessfully deleted.");
 			}
-			
+
 		}
 	}
 }
