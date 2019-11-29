f1dc78a3d64de5de10b516e45bd050c76f228400
Ildziite <ilga.baiza@gmail.com>
2019-11-29 20:26:39 +0200

    new weather forecast app

--- src/com/accenture/lecture15/copy/DayWeather.java
+++ src/com/accenture/lecture15/copy/DayWeather.java
@@ -1,6 +1,7 @@
 package com.accenture.lecture15.copy;
 
 import java.util.Date;
+import java.util.Random;
 
 public class DayWeather {
 	Date day;
@@ -8,12 +9,40 @@ public class DayWeather {
 	private int maxTemp;
 	private int minWind;
 	private int maxWind;
-	private String Wind;
+	private String wind;
 	
-	public DayWeather(){
-		
+	public DayWeather(int year, int month, int date){
+		this(year,month,date,-20,20,3,34,"N");
+	}
+	
+	public DayWeather(int year, int month, int date, int minTemp, int maxTemp, int minWind, int maxWind, String wind) {
+		@SuppressWarnings("deprecation")
+		Date day=new Date(year, month, date);
+		this.day=day;
+		this.minTemp=minTemp;
+		this.maxTemp=maxTemp;
+		this.minWind=minWind;
+		this.maxWind=maxWind;
+		this.wind=wind;
 	}
 
-	
+	public static DayWeather makeRandomForecast() {
+		Random rand = new Random();
+		
+		
+		int[] array = new int[7];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt((30 + 30) + 1) - 30;
+			// return r.nextInt((max - min) + 1) + min;
+		}
+		return new DayWeather(rand.nextInt((2020-2019)+1)+2019,rand.nextInt(12),rand.nextInt(30),rand.nextInt((30 + 30) + 1) - 30,rand.nextInt((30 + 30) + 1) - 30,rand.nextInt(30),rand.nextInt(30), "S");
+	}
+
+	@Override
+	public String toString() {
+		return "DayWeather [day=" + day + ", minTemp=" + minTemp + ", maxTemp="
+				+ maxTemp + ", minWind=" + minWind + ", maxWind=" + maxWind
+				+ ", wind=" + wind + "]";
+	}
 
 }
--- src/com/accenture/lecture15/copy/Weather.java
+++ src/com/accenture/lecture15/copy/Weather.java
@@ -8,153 +8,37 @@ public class Weather {
 
 	final String country;
 	ArrayList<DayWeather> weatherList;
-//	private int[] daysWeather;
-//	private String[] windWeather;
-	
-	public Weather(String country, int[] daysWeather) {
-		this.country = country;
-		this.daysWeather = daysWeather;
-	}
 
-	public Weather(String country, int[] daysWeather, String[] windWeather) {
+	public Weather(String country) {
 		this.country = country;
-		this.daysWeather = daysWeather;
-		this.windWeather = windWeather;
-	}
-
-	public int[] getDaysWeather() {
-		return daysWeather;
 	}
 
 	public String getCountry() {
 		return country;
 	}
 
-	public String[] getWindWeather() {
-		return windWeather;
+	public void addDayWeather(int year, int month, int date) {
+		DayWeather dayW = new DayWeather(year, month, date);
+		this.weatherList.add(dayW);
 	}
 
-	public String toString() {
-		return "Weather forecast in '" + this.country + "' for next days '"
-				+ Arrays.toString(this.getDaysWeather()) + "'. Wind forecast "+Arrays.toString(this.getWindWeather());
+	public void addRandomDayWeather(int year, int month, int date) {
+		DayWeather dayW = new DayWeather(year, month, date);
+		this.weatherList.add(dayW);
 	}
 
-	public double getWeekAverage() {
-		double sum = 0;
-		for (int i = 0; i < this.daysWeather.length; i++) {
-			sum = sum + this.daysWeather[i];
-		}
-		double average = sum / this.daysWeather.length;
-		return average;
-	}
-
-	private int getHottestDayIndex() {
-		int max = this.daysWeather[0];
-		int maxIndex = 0;
-		for (int i = 1; i < this.daysWeather.length; i++) {
-			if (max < this.daysWeather[i]) {
-				maxIndex = i;
-				max = this.daysWeather[i];
-			}
-		}
-		// if there is forecast for more then one week but it starts from Monday
-		maxIndex = maxIndex % 7;
-		return maxIndex;
-	}
-
-	public int getHottestTemperature() {
-		return this.daysWeather[getHottestDayIndex()];
-	}
-
-	public String getHottestDayName() {
-		return dayName(getHottestDayIndex());
-	}
-
-	private int getColdestDayIndex() {
-		int min = this.daysWeather[0];
-		int minIndex = 0;
-		for (int i = 1; i < this.daysWeather.length; i++) {
-			if (min > this.daysWeather[i]) {
-				minIndex = i;
-				min = this.daysWeather[i];
-			}
-		}
-		// if there is forecast for more then one week but it starts from Monday
-		minIndex = minIndex % 7;
-		return minIndex;
-	}
-
-	public int getColdestTemperature() {
-		return this.daysWeather[getColdestDayIndex()];
-	}
-
-	public String getColdestDayName() {
-		return dayName(getColdestDayIndex());
-	}
-
-	public void printAdvancedInformation() {
-		System.out.println("In " + this.country
-				+ " during next 7 day will be following weather "
-				+ Arrays.toString(this.daysWeather));
-		System.out.println("the highest temperature will be on "
-				+ getHottestDayName() + " " + getHottestTemperature()
-				+ " degree");
-		System.out.println("the lowest temperature will be on "
-				+ getColdestDayName() + " " + getColdestTemperature()
-				+ " degree");
-	}
-
-	public void applyWarmCycloneEffect() {
-		for (int i = 0; i < this.daysWeather.length; i++) {
-			this.daysWeather[i] += 1;
+	public void printForecast() {
+		System.out.println("Weather forecast in " + this.country + " for");
+		for (int i = 0; i < this.weatherList.size(); i++) {
+			System.out.println(this.weatherList.get(i));
 		}
 	}
 
-	private String dayName(int index) {
-		String dayName;
-		switch (index) {
-		case 0:
-			dayName = "Monday";
-			break;
-		case 1:
-			dayName = "Tuesday";
-			break;
-		case 2:
-			dayName = "Wednesday";
-			break;
-		case 3:
-			dayName = "Thursday";
-			break;
-		case 4:
-			dayName = "Friday";
-			break;
-		case 5:
-			dayName = "Saturday";
-			break;
-		case 6:
-			dayName = "Sunday";
-			break;
-		default:
-			dayName = "error";
-		}
-		return dayName;
-	}
+	// public String toString() {
+	// return "Weather forecast in '" + this.country + "' for next days '"
+	// + Arrays.toString(this.getDaysWeather()) +
+	// "'. Wind forecast "+Arrays.toString(this.getWindWeather());
+	// }
 
-	public static double celsiusToFahrenheit(double celsiusTemperature) {
-		return celsiusTemperature * 1.8 + 32;
-	}
-
-	public static Weather getWeatherForecast(String countryName) {
-		return new Weather(countryName, makeWeekForecastArray());
-	}
-
-	private static int[] makeWeekForecastArray() {
-		Random rand = new Random();
-		int[] array = new int[7];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = rand.nextInt((30 + 30) + 1) - 30;
-			// return r.nextInt((max - min) + 1) + min;
-		}
-		return array;
-	}
+	
 }
